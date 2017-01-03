dbbfb4fed5f69d0187e9f090d4b691a05ee281b5
Davide Gessa <gessadavide@gmail.com>
2017-01-03 18:07:44 +0100

    Projects update

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 var ligApp = angular.module ('ligApp', []);
 
 ligApp.controller('indexController', function indexController($scope, $http) {
+    $scope.projects = [];
     $scope.contributions = [];
     $scope.authors = {};
 
-    $http.get ('data/contributions.json').then (function (data) {
-        $scope.contributions = data.data;
+    $http.get ('data/projects.json').then (function (data) {
+        $scope.projects = data.data;
     });
 
     $http.get ('data/authors.json').then (function (data) {
         $scope.authors = data.data;
+    });
+
+    $http.get ('data/contributions.json').then (function (data) {
+        $scope.contributions = data.data;
     });
 });
